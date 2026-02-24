c4e430e66927c384ccab8a5f2256dc1af7b5491a
oniz93 <teo.miscia@gmail.com>
2026-02-24 22:26:21 +0000

    fix(infra): remove redundant CORS headers in Nginx and bump version to v1.5

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -23,7 +23,7 @@ class HexBattleApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-      title: 'Hex & Dice (v1.4)',
+      title: 'Hex & Dice (v1.5)',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
