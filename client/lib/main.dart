5545bd4b4d3727301ce5fb88073ed0f00372d929
oniz93 <teo.miscia@gmail.com>
2026-02-24 21:41:20 +0000

    fix(infra): restore api server block in nginx.conf and bump version to v1.2

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -23,7 +23,7 @@ class HexBattleApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-      title: 'Hex & Dice (v1.1)',
+      title: 'Hex & Dice (v1.2)',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
